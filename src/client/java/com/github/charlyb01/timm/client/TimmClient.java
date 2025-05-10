363d7a1d214306a27a7654f51d4fd48b7f35385d
charlyb01 <charly5bollinger@gmail.com>
2025-05-10 17:32:07 +0200

    Init fabric mod

---
+++ src/client/java/com/github/charlyb01/timm/client/TimmClient.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.timm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimmClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/github/charlyb01/timm/Timm.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.timm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Timm implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
