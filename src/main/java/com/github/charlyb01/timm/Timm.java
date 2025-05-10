e6b253a4a4b1b7e4fd16849c9087731643b16f2c
charlyb01 <charly5bollinger@gmail.com>
2025-05-10 18:30:25 +0200

    Add mod menu and cloth config dep

--- src/main/java/com/github/charlyb01/timm/Timm.java
+++ src/main/java/com/github/charlyb01/timm/Timm.java
@@ -1,10 +1,14 @@
 package com.github.charlyb01.timm;
 
+import com.github.charlyb01.timm.config.ModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 
 public class Timm implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
     }
 }
---
+++ src/main/java/com/github/charlyb01/timm/config/ModConfig.java
@@ -0,0 +1,12 @@
+package com.github.charlyb01.timm.config;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = "timm")
+public class ModConfig implements ConfigData {
+    public static ModConfig get() {
+        return AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+    }
+}
